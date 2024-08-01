1180e22123361d3edaadd3b357756ccbcef0298f
Norbert Niziołek <norbert@niziolek.dev>
2024-08-02 00:31:16 +0200

    Define question type

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,31 @@
-export interface Question {}
+interface QuestionBase {
+  type: string;
+  question: string;
+
+  /** Image URL */
+  image?: string;
+  title?: string;
+  description?: string;
+}
+
+interface SingleChoiceQuestion extends QuestionBase {
+  type: "single-choice";
+  options: string[];
+  correctIndex: number;
+}
+
+interface MultipleChoiceQuestion extends QuestionBase {
+  type: "multiple-choice";
+  options: string[];
+  correctIndices: number[];
+}
+
+interface InputQuestion extends QuestionBase {
+  type: "input";
+  correctAnswers: string[];
+}
+
+export type Question =
+  | SingleChoiceQuestion
+  | MultipleChoiceQuestion
+  | InputQuestion;
